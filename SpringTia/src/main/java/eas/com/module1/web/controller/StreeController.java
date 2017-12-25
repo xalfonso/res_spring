d24c04fab697115894ce04a0f53d7ffa69c39b30
Eduardo Alfonso <eddie.alfonso@gmail.com>
2017-12-25 13:12:51 -0500

    fin prevous test commit

--- SpringTia/src/main/java/eas/com/module1/web/controller/StreeController.java
+++ SpringTia/src/main/java/eas/com/module1/web/controller/StreeController.java
@@ -1,11 +1,10 @@
-Ipackage eas.com.module1.web.controller;
+package eas.com.module1.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by eduardo on 10/26/2016.
- Modificad
  */
 @RequestMapping(value = "stree")
 public interface StreeController extends Module1Controller{
