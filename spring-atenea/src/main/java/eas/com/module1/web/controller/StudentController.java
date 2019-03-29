76111a6e1b50e387cb7a26ba4cc392da01b209a0
Eduardo Alfonso <eddie.alfonso@gmail.com>
2019-03-29 10:08:13 -0500

    ...

--- spring-atenea/src/main/java/eas/com/module1/web/controller/StudentController.java
+++ spring-atenea/src/main/java/eas/com/module1/web/controller/StudentController.java
@@ -1,18 +1,21 @@
 package eas.com.module1.web.controller;
 
 import eas.com.common.entity.Student;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("student")
 @RestController
 public class StudentController {
 
     @GetMapping
-    public Student getStudent() {
+    public Student getStudent(HttpServletRequest httpServletRequest) {
         Student student = new Student();
-        student.setCountry("Ecuador");
+        student.setCountry(SecurityContextHolder.getContext().getAuthentication().toString() + " " + httpServletRequest.getSession().getId());
         return student;
     }
 }
