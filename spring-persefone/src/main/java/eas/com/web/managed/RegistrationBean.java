3c6a9833dceb4b366cc71b94edee64be53ce8947
Eduardo Alfonso <eddie.alfonso@gmail.com>
2019-04-01 17:18:33 -0500

    ...

--- spring-persefone/src/main/java/eas/com/web/managed/RegistrationBean.java
+++ spring-persefone/src/main/java/eas/com/web/managed/RegistrationBean.java
@@ -3,6 +3,7 @@ package eas.com.web.managed;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -15,7 +16,11 @@ public class RegistrationBean {
 
     private String userName;
     private String operationMessage;
+    private String userInfo;
 
+    public RegistrationBean() {
+        this.userInfo = SecurityContextHolder.getContext().getAuthentication().toString();
+    }
 
     public void createNewUser() {
        System.out.println("Call to createNewUser: " + this.userName);
---
+++ spring-persefone/src/main/java/eas/com/web/managed/SecurityKeyCloakManagedBean.java
@@ -0,0 +1,29 @@
+package eas.com.web.managed;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@Getter
+@Setter
+@ManagedBean
+@SessionScoped
+public class SecurityKeyCloakManagedBean {
+
+    private String refreshToken;
+
+    public SecurityKeyCloakManagedBean() {
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof KeycloakPrincipal) {
+                if (((KeycloakPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getKeycloakSecurityContext() instanceof RefreshableKeycloakSecurityContext) {
+                    this.refreshToken = ((RefreshableKeycloakSecurityContext) ((KeycloakPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getKeycloakSecurityContext()).getRefreshToken();
+                }
+            }
+        }
+    }
+}
